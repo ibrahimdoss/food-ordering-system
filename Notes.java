a8bef2736417e49568740ce6071fce80dac963a4
ibrahimdoss <ibrahimdos21@gmail.com>
2024-07-21 22:31:26 +0300

    change data capture before

--- Notes.java
+++ Notes.java
@@ -10,5 +10,5 @@ public class Notes {
     // relativePath: proje içerisindeki pom.xml dosyasının bulunduğu dizin ile parent pom.xml dosyasının bulunduğu dizin arasındaki ilişkiyi belirtir.
     // eğer parent pom.xml dosyası proje içerisindeki pom.xml dosyasının bulunduğu dizinde ise relativePath etiketi boş bırakılır.
     // eğer parent pom.xml dosyası proje içerisindeki pom.xml dosyasının bulunduğu dizinin bir üst dizininde ise relativePath etiketi "../" şeklinde belirtilir.
-
+//newt
 }
